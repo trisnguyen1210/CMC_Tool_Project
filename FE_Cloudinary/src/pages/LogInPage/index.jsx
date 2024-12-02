795186ea3db3bd4561f4afbec25973d94faff5a4
Tri <tri.nvm@cmctelecom.vn>
2024-12-02 14:35:17 +0700

    update UI

--- FE_Cloudinary/src/pages/LogInPage/index.jsx
+++ FE_Cloudinary/src/pages/LogInPage/index.jsx
@@ -1,58 +1,82 @@
-import { Facebook } from "lucide-react";
-import "./style.css";
-import { login } from "../../services/axios";
-import { useState } from "react";
+import "../../styles/Login.css";
+import { Button, Card, Form, Input, message } from "antd";
+import { useDispatch } from "react-redux";
+import { UserOutlined, LockOutlined } from "@ant-design/icons";
+import logo_trinvm from "../../assets/logo_trinvm.png";
+import { loginUser } from "../../redux/slices/user.slice";
 
 export default function LogInPage() {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-
-    const handleLogIn = async () => {
-        const responseUser = await login(username, password);
-        localStorage.setItem("token", responseUser.data);
-        if (responseUser.status === "success") {
-            window.location.reload();
-        }
+    const dispatch = useDispatch();
+    const handleLogIn = async (credentials) => {
+		try {
+			await dispatch(loginUser(credentials)).unwrap();
+			message.success('Đăng nhập thành công!');
+		} catch (error) {
+			message.error(error?.message || 'Đăng nhập thất bại')
+		}
     };
 
-    const handleKeyDown = async (event) => {
-        if (event.key === "Enter") {
-            handleLogIn();
-        }
-    };
     return (
         <div className="LogIn_background">
-            <div className="LogIn_form_background" onKeyDown={handleKeyDown}>
-                <h2 className="LogIn_form_title">Welcome to TriNVM Page</h2>
-                <div className="LogIn_form_input">
-                    <input
-                        id="username"
-                        className="LogIn_form_input_value"
-                        placeholder="Username"
-                        onChange={(e) => setUsername(e.target.value)}
-                    />
-                    <input
-                        id="password"
-                        className="LogIn_form_input_value"
-                        placeholder="Password"
-                        type="password"
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                </div>
-                <div className="LogIn_form_btn">
-                    <button id="btn" onClick={handleLogIn}>
-                        Đăng nhập
-                    </button>
-                </div>
-                <div className="horizontal_rule"></div>
-                <div>
-                    <h5>You get error?</h5>
-                    <br />
-                    <h5>Contact with TriNVM</h5>
-                </div>
-                <div className="LogIn_form_icon">
-                    <Facebook />
-                </div>
+            <div className="login-container">
+                <Card className="login-card" title="Đăng nhập" bordered={false}>
+                    <div className="logo-container">
+                        <img
+                            src={logo_trinvm}
+                            alt="TriNVM Logo"
+                            className="login-logo"
+                        />
+                    </div>
+                    <Form
+                        name="login"
+                        initialValues={{ remember: true }}
+                        layout="vertical"
+                        size="large"
+						autoComplete="off"
+                        onFinish={handleLogIn}
+                        onFinishFailed={(errorInfo) => {
+                            message.error('Vui lòng kiểm tra lại thông tin đăng nhập!');
+                        }}
+                    >
+                        <Form.Item
+                            name="username"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: "Vui lòng nhập username "
+                                }
+                            ]}
+                        >
+                            <Input
+                                placeholder="Username"
+                                prefix={<UserOutlined />}
+                            />
+                        </Form.Item>
+                        <Form.Item
+                            name="password"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: "Vui lòng nhập password"
+                                }
+                            ]}
+                        >
+                            <Input.Password
+                                prefix={<LockOutlined />}
+                                placeholder="Mật khẩu"
+                            />
+                        </Form.Item>
+                        <Form.Item>
+                            <Button
+                                className="login-button"
+                                type="primary"
+                                htmlType="submit"
+                            >
+                                Đăng nhập
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                </Card>
             </div>
         </div>
     );
--- FE_Cloudinary/src/redux/slices/user.slice.js
+++ FE_Cloudinary/src/redux/slices/user.slice.js
@@ -1,6 +1,6 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { login, tryGetUser } from "../services/axios";
-
+import { login, tryGetUser } from "../../services/axios";
+import { act } from "react";
 export const getUser = createAsyncThunk("api/user/getUser", async () => {
     const response = await tryGetUser();
     return response.data;
@@ -10,8 +10,8 @@ export const loginUser = createAsyncThunk(
     "api/user/login",
     async (credentials, { rejectWithValue }) => {
         try {
-            const response = await login(credentials);
-            localStorage.setItem("token", response.data.token);
+            const response = await login(credentials.username, credentials.password);            
+            localStorage.setItem("token", response.data);
             return response.data;
         } catch (error) {
             return rejectWithValue(error.response.data);
@@ -52,6 +52,8 @@ const userSlice = createSlice({
             .addCase(loginUser.fulfilled, (state, action) => {
                 state.isLoading = false;
                 state.isError = false;
+                console.log(action.payload);
+                
             })
             .addCase(loginUser.rejected, (state, action) => {
                 state.isLoading = false;
